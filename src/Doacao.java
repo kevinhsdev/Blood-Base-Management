0b72141279abecdb45ead6b61940bcd591ba713a
kevinhsdev <kevinhs07@outlook.com>
2026-05-14 09:24:22 -0300

    adiciona CGS nas classes Estoque e Doacao + Override toString em Doacao

--- src/Doacao.java
+++ src/Doacao.java
@@ -5,4 +5,49 @@ public class Doacao {
     private String data;
     private int idDoador;
 
+    public Doacao() {};
+
+    public Doacao(double quantidadeSangue, String data, int idDoador) {
+        this.quantidadeSangue = quantidadeSangue;
+        this.data = data;
+        this.idDoador = idDoador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getQuantidadeSangue() {
+        return quantidadeSangue;
+    }
+
+    public void setQuantidadeSangue(double quantidadeSangue) {
+        this.quantidadeSangue = quantidadeSangue;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getIdDoador() {
+        return idDoador;
+    }
+
+    public void setIdDoador(int idDoador) {
+        this.idDoador = idDoador;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] Doador ID: %d | %.0f ml | %s",
+                id, idDoador, quantidadeSangue, data);
+    }
 }
--- src/Estoque.java
+++ src/Estoque.java
@@ -2,4 +2,27 @@ public class Estoque {
 
     private String tipoSanguineo;
     private double quantidadeSangue;
+
+    public Estoque() {};
+
+    public Estoque(String tipoSanguineo, double quantidadeSangue) {
+        this.tipoSanguineo = tipoSanguineo;
+        this.quantidadeSangue = quantidadeSangue;
+    }
+
+    public String getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+
+    public void setTipoSanguineo(String tipoSanguineo) {
+        this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public double getQuantidadeSangue() {
+        return quantidadeSangue;
+    }
+
+    public void setQuantidadeSangue(double quantidadeSangue) {
+        this.quantidadeSangue = quantidadeSangue;
+    }
 }
