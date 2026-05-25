c72d23a5087599901c1cba218317c51a4f071533
kevinhsdev <kevinhs07@outlook.com>
2026-05-25 09:37:29 -0300

    acrescenta codigo nas classes DoadorDAO e ConnectionFactory

--- src/Connection/ConnectionFactory.java
+++ src/Connection/ConnectionFactory.java
@@ -1,5 +1,18 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
+    private static final String URL      = "jdbc:mysql://localhost:3306/hemocentro";
+    private static final String USUARIO  = "root";
+    private static final String SENHA    = "@Capitaok104864";
+
+    // Metodo estático — qualquer DAO chama ConnectionFactory.getConnection()
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+
+    }
 }
