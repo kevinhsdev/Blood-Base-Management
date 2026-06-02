03e056d74e6ebaf8a64f7899774b37bed6b2639c
Kevin Henrique Silva <kevinhs07@outlook.com>
2026-06-02 14:06:23 -0300

    Obscure MySQL password in ConnectionFactory

--- src/Util/ConnectionFactory.java
+++ src/Util/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
     private static final String URL      = "jdbc:mysql://localhost:3306/hemocentro";
     private static final String USUARIO  = "root";
-    private static final String SENHA    = "@Capitaok104864";
+    private static final String SENHA    = "**************"; //colocar senha do MySQL
 
     // Metodo estático — qualquer DAO chama ConnectionFactory.getConnection()
     public static Connection getConnection() throws SQLException {
